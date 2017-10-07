aaa6dfdeafb2569944e719c116f73ca0ecd97018
Axel Bojrup <12974578@ad.feit.uts.edu.au>
2017-10-07 17:37:40 +1100

    added skeleton for Guest

--- Assignment2/Assignment2/Guest.cs
+++ Assignment2/Assignment2/Guest.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
-    class Guest
+    class Guest : User
     {
+        private DateTime dateOfBirth;
+
+        public Guest(string username, string password, string firstName, string lastName, int ratingsCount, float averageRating, DateTime dateOfBirth)
+        {
+            this.username = username;
+            this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.ratingsCount = ratingsCount;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+// writes all member variable of this Admin object (including those in the User  base  class)    to the  end of  the file  passed  in  as  a parameter  and returns true if the write was successful.
+        public bool WriteGuestToFile(System.IO.StreamWriter file)
+        {
+            return true;
+        }
+
+// overrides  base  class  version, returns a string with firstName, lastName, and dateOfBirth.
+        public override string GetFullUserString()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
