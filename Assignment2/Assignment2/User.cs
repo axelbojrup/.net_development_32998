a49b415f092e6eaea98e34b6ca9f6bf0891a865a
Axel Bojrup <12974578@ad.feit.uts.edu.au>
2017-10-07 16:59:08 +1100

    added skeleton code for User

--- Assignment2/Assignment2/User.cs
+++ Assignment2/Assignment2/User.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
-    class User
+    abstract class User
     {
+        protected string username, password, firstName, lastName;
+        protected int ratingsCount;
+
+        public int RatingsCount
+        {
+            get
+            {
+                return ratingsCount;
+            }
+        }
+
+        protected double averageRating;
+
+        public double AverageRating
+        {
+            get
+            {
+                return averageRating;
+            }
+        }
+
+// concrete  method,  checks  the  username  and  password parameters against  the member  variables and  return  true  if  they match and returns false otherwise.
+        public bool CheckUserNameAndPassword(string username, string password)
+        {
+            return true;
+        }
+// concrete method, returns astring containing the username and first name only.
+        public string GetShortUserString()
+        {
+            return "";
+        }
+// concrete method, updates the averageRating and ratingsCount member variables with the given parameter.
+        public void AddRating(int rating)
+        {
+
+        }
+
+        public abstract string GetFullUserString();
     }
 }
