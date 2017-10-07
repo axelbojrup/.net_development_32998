a8b9a488d8785223bf1d51a5970fd0533d143372
Axel Bojrup <12974578@ad.feit.uts.edu.au>
2017-10-07 17:50:47 +1100

    added skeleton code for admin, need to resolve Type conversion for AdminType see commented code

--- Assignment2/Assignment2/Admin.cs
+++ Assignment2/Assignment2/Admin.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
-    class Admin
+    class Admin : User
     {
+        enum AdminType { SuperAdmin, Moderator};
+
+        private AdminType adminType;
+
+        public Admin(string username, string password, string firstName, string lastName, int ratingsCount, float averageRatingAdmin, Type AdminType)
+        {
+            this.username = username;
+            this.password = password;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.ratingsCount = ratingsCount;
+            this.averageRating = averageRatingAdmin;
+            // this.adminType = AdminType;
+            // https://stackoverflow.com/questions/30405515/c-sharp-error-message-cannot-implicitly-convert-type
+        }
+// writes all member variable of this Admin object (including those in the        User  base  class)  to the  end of  the file  passed  in  as a parameter  and returns true if the write was successful
+        public bool WriteAdminToFile(System.IO.StreamWriter file)
+        {
+            return true;
+        }
+        public override string GetFullUserString()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
